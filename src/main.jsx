8a4115b3cd92f1bed808bfa25998fe1acb625ffd
Nazisg <isgnezrin@gmail.com>
2024-03-15 22:00:13 +0400

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.scss";
-import 'tailwindcss/tailwind.css';
-import "tw-elements-react/dist/css/tw-elements-react.min.css";
-import { store,persistor  } from "./redux/app/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import 'tailwindcss/tailwind.css';
+import "tw-elements-react/dist/css/tw-elements-react.min.css";
+import App from "./App.jsx";
+import "./index.scss";
+import { persistor, store } from "./redux/app/store.js";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
